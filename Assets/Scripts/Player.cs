47010412a1b32b22ddc574e9c5a7d5d16b90b83d
Hiigaaran <simo.moralesg@gmail.com>
2016-01-11 14:58:05 -0300

    Proyecto base Helados York

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Salto de Player
+		if (Input.GetKeyUp("space")) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2(0,300));
+		}
+		//Avance a la derecha.
+		if (Input.GetKey("right")) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2(50,0));
+		}
+		//Avance izquierda
+		if (Input.GetKey("left")) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2(-50,0));
+		}
+
+		//posible poder
+		/*if (Input.GetKeyUp("alt")) {
+			
+		}*/
+	}
+}
