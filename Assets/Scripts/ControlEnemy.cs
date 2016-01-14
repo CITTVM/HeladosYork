20f7bc7cc444b7798f0776ea853bd21fe4446a40
Dialetelier <dialetelier@gmail.com>
2016-01-14 04:46:28 -0300

    Actualizacion codigo colision, con error aun
    Lo subo para que ignacio me ayude.
    Simon si puedes ojearlo, hize eso de los tags pero no pasa nadal.

---
+++ Assets/Scripts/ControlEnemy.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlEnemy : MonoBehaviour {
+	// PARA LA COLICION
+
+	public GameManager gameManager;
+
+
+	int damageValue = 1;
+	// GENERAR COLISION
+	void OnTriggerEnter(Collider col){
+
+		if(col.gameObject.tag == "player_fly_f01"){
+			gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
+			gameManager.controlPlayer.SendMessage ("TakenDamage", SendMessageOptions.DontRequireReceiver);
+
+		}
+	}
+
+
+
+	// MOVIMIENTO DERECHA A IZQUIERDA DEL ENEMIGO
+	float inicioPos;
+	float finPos;
+
+	public int unitsToMove = 5;
+	public int moveSpeed = 2;
+	bool moveRight =true;
+
+	void Awake(){
+
+		inicioPos = transform.position.x;
+		finPos = inicioPos + unitsToMove;
+	}
+
+
+
+	void Update () {
+
+		if (moveRight){
+
+			GetComponent<Rigidbody2D>().position += Vector2.right * moveSpeed * Time.deltaTime;
+		}
+
+		if(GetComponent<Rigidbody2D>().position.x >= finPos){
+			moveRight = false;
+
+		}
+
+		if (!moveRight) {
+
+			GetComponent<Rigidbody2D>().position -= Vector2.right * moveSpeed * Time.deltaTime;
+		}
+
+
+		if(GetComponent<Rigidbody2D>().position.x <= inicioPos){
+
+			moveRight = true;
+		}
+
+
+
+
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+	
+	public ControlPlayer controlPlayer;
+	// Use this for initialization
+
+	//controlar la textura del player
+	public Texture playersHealthTexture;
+
+	public float screenPositionX;
+	public float screenPositionY;
+
+	public float iconSizeX = 25;
+	public float iconSizeY = 25;
+
+	//iniciara con 3 health
+	public int playersHealth = 3;
+
+
+
+
+
+	//para quitar health
+	void OnGUI(){
+		for(int h = 0; h < playersHealth; h++) {
+			GUI.DrawTexture(new Rect(screenPositionX + (h * iconSizeX), screenPositionY, iconSizeX, iconSizeY), playersHealthTexture, ScaleMode.ScaleToFit, true, 0);
+		}
+
+	}
+	//metodo del damage 
+	void PlayerDamaged(int damage)
+	{
+
+		if (playersHealth > 0) {
+
+			playersHealth -= damage;
+		}
+
+		if(playersHealth <= 0){
+			playersHealth = 0;
+			RestartScene();
+
+		}
+
+	}
+
+	void RestartScene(){
+
+		SceneManager.LoadScene("Test");
+
+	}
+
+
+
+}
