ac1c2b747522fe99a0a5b3678d4c316b402a020f
Hiigaaran <simo.moralesg@gmail.com>
2016-01-14 20:38:26 -0300

    Barra de Hidratacion

--- Assets/Scripts/ControlPlayer.cs
+++ Assets/Scripts/ControlPlayer.cs
@@ -15,9 +15,14 @@ public class ControlPlayer : MonoBehaviour
 	//takendamage
 	float takenDamage = 0.2f;
 
-
-
-
+	#region Barra de Hidratacion
+	public RectTransform HidroTransform;
+	private float almacenadaY;
+	private float minXValue;
+	private float maxXValue;
+	private int hidratacionActual;
+	public int MaxHidratacion;
+	#endregion
 
 	//Variables enSuelo
 
@@ -25,6 +30,15 @@ public class ControlPlayer : MonoBehaviour
 	public Transform comprobadorSuelo;
 	float comprobadorRadio = 1.5f;
 	public LayerMask mascaraSuelo;
+
+	void Start(){
+		#region Barra de Hidratacion
+		almacenadaY = HidroTransform.position.y;
+		maxXValue = HidroTransform.position.x;
+		minXValue = HidroTransform.position.x - HidroTransform.rect.width;
+		setHidratacionActual(MaxHidratacion);
+		#endregion
+	}
 
 	void FixedUpdate(){
 		enSuelo = Physics2D.OverlapCircle (comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
@@ -55,7 +69,14 @@ public class ControlPlayer : MonoBehaviour
 
 		GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveVelocity, GetComponent<Rigidbody2D> ().velocity.y);
 
+		#region Barra de Hidratacion
+		if (hidratacionActual >= 0) {
+			//HidratacionActual = (int)(HidratacionActual - (1 * Time.deltaTime));
+			setHidratacionActual((int)(getHidratacionActual() - (0.9f * Time.deltaTime)));
+		}
+		#endregion
 	}
+
 	//Chequear ensuelo
 	void OnTriggerEnter2D()
 	{
@@ -81,4 +102,30 @@ public class ControlPlayer : MonoBehaviour
 		GetComponent<Renderer>().enabled = true;
 		yield return new WaitForSeconds(takenDamage);
 	}
+	#region Barra de Hidratacion	
+	private void ManejoHidratacion(){
+		float ValorXActual = MapeoValores (getHidratacionActual(), 0, MaxHidratacion, minXValue, maxXValue);
+		HidroTransform.position = new Vector2 (ValorXActual, almacenadaY);
+	}
+
+	private float MapeoValores(float x, float inMin, float inMax, float outMin, float outMax){
+		return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
+	}
+
+	/*private int HidratacionActual{
+		get { return HidratacionActual;}
+		set { 
+			HidratacionActual = value;
+			ManejoHidratacion ();
+		}
+	}*/
+
+	private void setHidratacionActual(int value){
+		hidratacionActual = value;
+		ManejoHidratacion ();
+	}
+	private int getHidratacionActual(){
+		return hidratacionActual;
+	}
+	#endregion
 }
