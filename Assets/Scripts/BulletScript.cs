07e5802dfc6894c505fe9eec8d070dc8d90c9fd3
Dialetelier <dialetelier@gmail.com>
2016-01-17 19:51:01 -0300

    Jugador Ataca y Enemigo muere
    El jugador tira un proyectil (cuadrado) que cuando colisiona con el
    enemigo  lo mata.

---
+++ Assets/Scripts/BulletScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour {
+
+
+	void OnTriggerEnter2D (Collider2D col)
+	{
+
+		if (col.gameObject.tag == "Enemy") {
+			Destroy (gameObject);
+			Destroy (col.gameObject);
+		
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/PlayerAtack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAtack : MonoBehaviour {
+
+	// Use this for initialization
+	public Rigidbody2D bulletPrefab;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.F))
+			BulletAttack ();
+	
+	}
+
+	void BulletAttack(){
+
+		Rigidbody2D bPrefab = Instantiate(bulletPrefab,transform.position,Quaternion.identity) as Rigidbody2D;
+		bPrefab.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 500);
+	}
+
+
+}
