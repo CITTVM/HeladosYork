b30e53620ce345e738f8f4d0583fa70dd61dacac
Hiigaaran <simo.moralesg@gmail.com>
2016-01-26 17:47:07 -0300

    Prototipo Menu Principal Funcional

---
+++ Assets/Scripts/MenuPrincipal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuPrincipal : MonoBehaviour{
+
+    public string startLevel;
+    
+    public void NewGame()
+    {
+        SceneManager.LoadScene(startLevel);
+    }
+
+    
+}
