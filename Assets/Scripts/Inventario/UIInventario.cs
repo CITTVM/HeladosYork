2d076e3c12bbe6704c674d28574a4be14f2cf1b5
Dialetelier <dialetelier@gmail.com>
2016-01-29 13:29:18 -0300

    Inventario incompleto
    Lo unico que falta es que cuando el jugador toque un helado este se
    almacene en el inventario.
    Las armas (helados) se cambian con los numeros 1 y 2

---
+++ Assets/Scripts/Inventario/UIInventario.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIInventario : MonoBehaviour {
+
+//    public Color[] colorSlots;
+//    public Color[] colorIconos;
+//
+//    [SerializeField]public Imagenes imagenes;
+//    [System.Serializable]
+//    public class Imagenes
+//    {
+//        public Image[] slot, icono;
+//    }
+//    [SerializeField]
+//    Iconos iconos;
+//    [System.Serializable]public class Iconos {
+//        public Sprite pistola, medicina, linterna;
+//    }
+//    
+//
+//	
+//	private GameObject scriptsContenedor;
+//	private JugadorBD jugadorBD;
+//    [HideInInspector]public Inventario inventario;
+//
+//
+//
+//	// Use this for initialization
+//	void Awake () {
+//		
+//		scriptsContenedor=GameObject.FindGameObjectWithTag ("Scripts");
+//		jugadorBD=scriptsContenedor.GetComponent<JugadorBD>();
+//        inventario = scriptsContenedor.GetComponent<Inventario>();
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		for(int i = 0; i < imagenes.icono.Length; i++)
+//        {
+//            if(jugadorBD.SlotsBasicos[i] == "Medicina")
+//            {
+//                imagenes.icono[i].sprite = iconos.medicina;
+//                imagenes.icono[i].enabled = true;
+//            }
+//            if (jugadorBD.SlotsBasicos[i] == "Pistola")
+//            {
+//                imagenes.icono[i].sprite = iconos.pistola;
+//                imagenes.icono[i].enabled = true;
+//            }
+//            if (jugadorBD.SlotsBasicos[i] == "Linterna")
+//            {
+//                imagenes.icono[i].sprite = iconos.linterna;
+//                imagenes.icono[i].enabled = true;
+//            }
+//            if (jugadorBD.SlotsBasicos[i] == "")
+//            {
+//                imagenes.icono[i].enabled = false;
+//            }
+//        }
+//        for(int j = 0; j < imagenes.slot.Length; j++)
+//        {
+//            if (j == inventario.slotActual)
+//            {
+//                imagenes.slot[inventario.slotActual].color = colorSlots[0];
+//                imagenes.icono[inventario.slotActual].color = colorIconos[0];
+//            }
+//            else
+//            {
+//                imagenes.slot[j].color = colorSlots[1];
+//                imagenes.icono[j].color = colorIconos[1];
+//
+//            }
+//        }
+//	}
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+
+	public enum WeaponType
+	{
+		Sword,
+		Gun
+	};
+
+	public WeaponType weaponType;
+	public Texture weaponLogo = null;
+	public float dmg = 0;
+
+
+
+}
+
+
