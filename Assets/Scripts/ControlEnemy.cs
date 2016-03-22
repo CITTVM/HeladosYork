f6ddbdc58175f86cde2e93651adcf73a260f053e
willywes <willywes_69@hotmail.com>
2016-03-22 13:47:42 -0300

    Patrullajes enemigos, mejorados, hitbox

--- Assets/Scripts/ControlEnemy.cs
+++ Assets/Scripts/ControlEnemy.cs
@@ -1,78 +1,82 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ControlEnemy : MonoBehaviour {
-	
-	//referencia a GameManager
-	public GameManager gameManager;
+public class ControlEnemy : MonoBehaviour
+{
+
+    //referencia a GameManager
+    public GameManager gameManager;
 
 
 
 
-	//establezco el daño que recibira
-	int damageValue = 1;
+    //establezco el daño que recibira
+    int damageValue = 1;
 
 
 
-	// GENERAR COLISION
-	// Si colisiona el enemigo (ontrigger porque el enemigo tiene un boxcollider2d con ontrigger activado)
-	// se produce el metodo de que toma daño y el metodo que quita vida
+    // GENERAR COLISION
+    // Si colisiona el enemigo (ontrigger porque el enemigo tiene un boxcollider2d con ontrigger activado)
+    // se produce el metodo de que toma daño y el metodo que quita vida
 
 
-	void OnTriggerEnter2D(Collider2D col){
+    void OnTriggerEnter2D(Collider2D col)
+    {
 
-		if(col.gameObject.tag == "Player"){
-			gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
-            gameManager.controlPlayer.SendMessage ("TakenDamage", SendMessageOptions.DontRequireReceiver);
+        if (col.gameObject.tag == "Player")
+        {
+            gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
+            gameManager.controlPlayer.SendMessage("TakenDamage", SendMessageOptions.DontRequireReceiver);
 
-		}
-	}
+        }
+
+        if (col.gameObject.tag == "Hitbox")
+        {
+            if (moveRight)
+            {
+                moveRight = false;
+            }
+            else
+            {
+                moveRight = true;
+            }
+
+        }
+    }
 
 
 
-	// MOVIMIENTO DERECHA A IZQUIERDA DEL ENEMIGO
-	float inicioPos;
-	float finPos;
+    // MOVIMIENTO DERECHA A IZQUIERDA DEL ENEMIGO
+ /*  float inicioPos;
+    float finPos;*/
 
-	public int unitsToMove = 5;
-	public int moveSpeed = 2;
-	bool moveRight =true;
+  //  public int unitsToMove = 5;
+    public int moveSpeed = 10;
+    bool moveRight = true;
 
-	void Awake(){
+    void Awake()
+    {
 
-		inicioPos = transform.position.x;
-		finPos = inicioPos + unitsToMove;
-	}
+      //  inicioPos = transform.position.x;
+       
+    }
 
     void Update()
     {
+
+        
+
+        if (!moveRight)
+        {
+            GetComponent<Rigidbody2D>().position -= Vector2.right * moveSpeed * Time.deltaTime;
+            transform.eulerAngles = new Vector2(0, 180);
+        }
 
         if (moveRight)
         {
 
             GetComponent<Rigidbody2D>().position += Vector2.right * moveSpeed * Time.deltaTime;
             transform.eulerAngles = new Vector2(0, 0);
-        }
-
-        if (GetComponent<Rigidbody2D>().position.x >= finPos)
-        {
-            moveRight = false;
-
-        }
-
-        if (!moveRight)
-        {
-
-
-            GetComponent<Rigidbody2D>().position -= Vector2.right * moveSpeed * Time.deltaTime;
-            transform.eulerAngles = new Vector2(0, 180);
-        }
-
-
-        if (GetComponent<Rigidbody2D>().position.x <= inicioPos)
-        {
-
-            moveRight = true;
         }
 
 
