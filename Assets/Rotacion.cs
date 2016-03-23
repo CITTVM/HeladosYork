c78fedb46df36dad91a8cc4ee7c0abe24fc30fab
Hiigaaran <simo.moralesg@gmail.com>
2016-03-23 17:50:40 -0300

    barra giratoria que hace daño commit de jesus

---
+++ Assets/Rotacion.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotacion : MonoBehaviour {
+    //referencia a GameManager
+    public GameManager gameManager;
+
+
+
+
+    //establezco el daño que recibira
+    int damageValue = 1;
+
+
+
+    // GENERAR COLISION
+    // Si colisiona el enemigo (ontrigger porque el enemigo tiene un boxcollider2d con ontrigger activado)
+    // se produce el metodo de que toma daño y el metodo que quita vida
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
+            gameManager.controlPlayer.SendMessage("TakenDamage", SendMessageOptions.DontRequireReceiver);
+
+        }
+
+    }
+     void Update () 
+    {
+        RotacionObstaculo();
+	}
+
+
+
+     void RotacionObstaculo()
+    {
+        transform.Rotate(Vector3.forward * -10);
+    }
+}
