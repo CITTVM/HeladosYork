bebfc09639b58e2c98d91c71f642a0d1fa3d599b
matamalaDDG <smatamala92@gmail.com>
2016-04-15 11:30:54 -0300

    fix daño barra giratoriada

--- Assets/Rotacion.cs
+++ Assets/Rotacion.cs
@@ -3,36 +3,55 @@ using System.Collections;
 
 public class Rotacion : MonoBehaviour {
     //referencia a GameManager
-    public GameManager gameManager;
-
-
+    public ControlPlayer gameManager;
 
 
     //establezco el daño que recibira
     int damageValue = 1;
+    //Variable Booleana para controlar cada tanto tiempo que al estar colisionando con el obstaculo dañará al player
+    bool Dañando = true;
+
+    //referencia a GameManager
 
 
-
-    // GENERAR COLISION
-    // Si colisiona el enemigo (ontrigger porque el enemigo tiene un boxcollider2d con ontrigger activado)
-    // se produce el metodo de que toma daño y el metodo que quita vida
-    // Use this for initialization
-    void Start () {
-	
-	}
-
-    // Update is called once per frame
-    void OnTriggerEnter2D(Collider2D col)
+    void Awake()
     {
-
-        if (col.gameObject.tag == "Player")
-        {
-            gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
-            gameManager.controlPlayer.SendMessage("TakenDamage", SendMessageOptions.DontRequireReceiver);
-
-        }
+        gameManager = GameObject.Find("Player").GetComponent<ControlPlayer>();
+    }
+    void Start() {
 
     }
+
+    // Update is called once per frame
+    void OnTriggerStay2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player" && Dañando)
+        {
+            Dañando = false;
+            gameManager.SendMessage("PlayerDamaged", damageValue, SendMessageOptions.DontRequireReceiver);
+            gameManager.SendMessage("TakenDamage", SendMessageOptions.DontRequireReceiver);
+            //Activo la rutina para desactivar el boolean
+            StartCoroutine("Reactivar_Daño");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+            //Activo la rutina para desactivar el boolean
+            StartCoroutine("Reactivar_Daño");
+        
+    }
+
+
+    //el Yield return devolverá el tiempo que es necesario para volver a activar la booleana.
+    IEnumerator Reactivar_Daño()
+    {
+        yield return new WaitForSeconds(1);
+        Dañando = true;
+    }
+
+    //Rotacion.
      void Update () 
     {
         RotacionObstaculo();
